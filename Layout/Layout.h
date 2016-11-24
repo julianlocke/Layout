48d19bdb6a82f0ca8007b4a9a42a91e22af7c9de
Cameron Pulsford <cpulsfor@me.com>
2016-11-24 06:50:30 -0500

    Initial Commit

---
+++ Layout/Layout.h
@@ -0,0 +1,19 @@
+//
+//  Layout.h
+//  Layout
+//
+//  Created by Cameron Pulsford on 11/24/16.
+//  Copyright © 2016 Savant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Layout.
+FOUNDATION_EXPORT double LayoutVersionNumber;
+
+//! Project version string for Layout.
+FOUNDATION_EXPORT const unsigned char LayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Layout/PublicHeader.h>
+
+
