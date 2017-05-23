1cbb5f87a7bfbe0ea8083419f5197b11adae3528
Cameron Pulsford <cpulsfor@me.com>
2017-05-22 22:49:00 -0400

    Actually improves type safety of <=.

--- Layout/Source/LayoutDescriptorOperators.swift
+++ Layout/Source/LayoutDescriptorOperators.swift
@@ -121,7 +121,7 @@ public func <= <Kind>(lhs: LayoutDescriptor<Kind>, rhs: ConstraintContainer) -> 
     }
 }
 
-public func <= <Kind>(lhs: LayoutDescriptor<Kind>, rhs: CGFloat) -> LayoutDescriptor<Kind> {
+public func <= (lhs: LayoutDescriptor<DimensionLayout>, rhs: CGFloat) -> LayoutDescriptor<DimensionLayout> {
     return lhs.modify { result in
         result.constant = rhs
         result.multiplier = 1
